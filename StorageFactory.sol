f681f698246680fbe781abc518c499c7a80d5c34
“[ishaq <[isharqvip@gmai.com]>
2025-08-24 01:28:32 -0700

    Second

---
+++ StorageFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.30;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContracts() public {
+        SimpleStorage mySimpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(mySimpleStorage);
+    }
+
+    function fsStore(uint256 _indexNumber, uint256 _newNumber) public {
+        listOfSimpleStorageContracts[_indexNumber].store(_newNumber);
+    }
+
+    function fsGet(uint256 _newNumber) public view {
+        listOfSimpleStorageContracts[_newNumber].retrieve();
+    }
+}
